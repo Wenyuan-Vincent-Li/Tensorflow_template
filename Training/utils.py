828ac62897d9abc282c71a1b3a093a71a1317c13
wenyuan <liwenyuan.zju@gmail.com>
2018-09-04 18:24:24 -0400

    import module to solve the model-base bug add more functions to the training/utils

--- Training/utils.py
+++ Training/utils.py
@@ -12,12 +12,35 @@ import tensorflow as tf
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import clip_ops
 from tensorflow.python.tools import inspect_checkpoint as chkp
+from tensorflow.python.client import device_lib
+
+def get_available_gpus():
+  local_device_protos = device_lib.list_local_devices()
+  return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
 def variable_name_string():
     name_string = ''
     for v in tf.global_variables():
         name_string += v.name + '\n'
     return name_string
+
+
+def get_trainable_weight_num(var_list = tf.trainable_variables()):
+  total_parameters = 0
+  for variable in var_list:
+    # shape is an array of tf.Dimension
+    shape = variable.get_shape()
+    variable_parameters = 1
+    for dim in shape:
+      variable_parameters *= dim.value
+    total_parameters += variable_parameters
+  return total_parameters
+
+def variable_name_string_specified(variables):
+  name_string = ''
+  for v in variables:
+    name_string += v.name + '\n'
+  return name_string
 
 def grads_dict(gradients, histogram_dict):
     
@@ -38,6 +61,32 @@ def fn_inspect_checkpoint(ckpt_filepath, **kwargs):
     else:
         all_tensors = False    
     chkp.print_tensors_in_checkpoint_file(ckpt_filepath, name, all_tensors)
+
+
+def add_grads(grads, histogram_dict):
+    for index, grad in enumerate(grads):
+        tf.summary.histogram("{}-grad".format(grads[index][1].name), grads[index])
+
+
+def initialize_uninitialized_vars(sess):
+    from itertools import compress
+    global_vars = tf.global_variables()
+    local_vars = tf.local_variables()
+    init_var = global_vars + local_vars
+    is_not_initialized = sess.run([~(tf.is_variable_initialized(var)) \
+                                   for var in init_var])
+    not_initialized_vars = list(compress(global_vars, is_not_initialized))
+
+    if len(not_initialized_vars):
+        sess.run(tf.variables_initializer(not_initialized_vars))
+
+    local_vars = tf.local_variables()
+    is_not_initialized = sess.run([~(tf.is_variable_initialized(var)) \
+                                   for var in local_vars])
+    not_initialized_vars = list(compress(local_vars, is_not_initialized))
+
+    if len(not_initialized_vars):
+        sess.run(tf.variables_initializer(not_initialized_vars))
     
 ## Test code
 def _main_variable_name_string():
